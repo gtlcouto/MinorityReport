0552dd487b7a4cffa9fbb60fac401f5c72774640
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-01-08 10:33:33 -0800

    Initial Commit

---
+++ MinorityReport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MinorityReport
+//
+//  Created by Gustavo Couto on 2015-01-08.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MinorityReport/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MinorityReport
+//
+//  Created by Gustavo Couto on 2015-01-08.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
